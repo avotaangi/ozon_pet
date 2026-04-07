4428c46b5ad6aeb997904fa350c17c745c19b4a7
avotaangi <alinaignatova67@gmail.com>
2026-04-08 01:43:33 +0300

    Scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import { AppShell } from './components/layout/AppShell'
 import { CatalogPage } from './pages/CatalogPage'
 import { BreedProfilesPage } from './pages/BreedProfilesPage'
@@ -16,151 +17,161 @@ import { ServiceDetailPage } from './pages/ServiceDetailPage'
 import { ServicesPage } from './pages/ServicesPage'
 import { ServiceBookingPage } from './pages/ServiceBookingPage'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <AppShell>
-            <MarketplaceHomePage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/catalog"
-        element={
-          <AppShell>
-            <CatalogPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/product/:itemId"
-        element={
-          <AppShell>
-            <ProductDetailPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/services"
-        element={
-          <AppShell>
-            <ServicesPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/service/:itemId"
-        element={
-          <AppShell>
-            <ServiceDetailPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/booking/:kind/:itemId"
-        element={
-          <AppShell>
-            <ServiceBookingPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <AppShell>
-            <LoginPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/pet-click"
-        element={
-          <AppShell>
-            <PetClickPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/petpro"
-        element={
-          <AppShell>
-            <PetProPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/pet-id"
-        element={
-          <AppShell>
-            <PetIdPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/pet-id/:petId"
-        element={
-          <AppShell>
-            <PetIdPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/pet-id/:petId/community"
-        element={
-          <AppShell>
-            <BreedProfilesPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/pet-dobro"
-        element={
-          <AppShell>
-            <PetDobroPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/pet-dobro/:profileSlug/campaigns"
-        element={
-          <AppShell>
-            <PetDobroPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/pet-dobro/:profileSlug"
-        element={
-          <AppShell>
-            <PetDobroPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/pet-blog"
-        element={
-          <AppShell>
-            <PetBlogPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/pet-subscription"
-        element={
-          <AppShell>
-            <PetSubscriptionPage />
-          </AppShell>
-        }
-      />
-      <Route
-        path="/ecosystem"
-        element={<EcosystemPage />}
-      />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AppShell>
+              <MarketplaceHomePage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/catalog"
+          element={
+            <AppShell>
+              <CatalogPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/product/:itemId"
+          element={
+            <AppShell>
+              <ProductDetailPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/services"
+          element={
+            <AppShell>
+              <ServicesPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/service/:itemId"
+          element={
+            <AppShell>
+              <ServiceDetailPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/booking/:kind/:itemId"
+          element={
+            <AppShell>
+              <ServiceBookingPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <AppShell>
+              <LoginPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/pet-click"
+          element={
+            <AppShell>
+              <PetClickPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/petpro"
+          element={
+            <AppShell>
+              <PetProPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/pet-id"
+          element={
+            <AppShell>
+              <PetIdPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/pet-id/:petId"
+          element={
+            <AppShell>
+              <PetIdPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/pet-id/:petId/community"
+          element={
+            <AppShell>
+              <BreedProfilesPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/pet-dobro"
+          element={
+            <AppShell>
+              <PetDobroPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/pet-dobro/:profileSlug/campaigns"
+          element={
+            <AppShell>
+              <PetDobroPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/pet-dobro/:profileSlug"
+          element={
+            <AppShell>
+              <PetDobroPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/pet-blog"
+          element={
+            <AppShell>
+              <PetBlogPage />
+            </AppShell>
+          }
+        />
+        <Route
+          path="/pet-subscription"
+          element={
+            <AppShell>
+              <PetSubscriptionPage />
+            </AppShell>
+          }
+        />
+        <Route path="/ecosystem" element={<EcosystemPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   )
 }
 
